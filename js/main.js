335d3b04b18a00af886eac03cf2a005e79e6d82c
alan <alkalinemusic@gmail.com>
2020-05-05 08:46:39 -0400

    adjust navbar position, color, size

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-alert("YOU HACKED IT");
-
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
