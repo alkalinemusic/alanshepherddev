260aaa366bc2646d3c9b40c191d5c16459e21869
alan <alkalinemusic@gmail.com>
2020-05-09 10:33:31 -0400

    remove unnecessary comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
 // sliding menu and burger icon animation
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
