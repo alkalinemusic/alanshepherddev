8bfb9478d1d26a8fdb89b20f7fd11d7b739cbc82
alan <alkalinemusic@gmail.com>
2020-05-09 10:22:30 -0400

    implement new horizontal menu with slide in small screen mode

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+// sliding menu and burger icon animation
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		//toggle nav
+		nav.classList.toggle('nav-active');
+		//animate links
+		navLinks.forEach((link, index)=>{
+			if (link.style.animation) {
+				link.style.animation = ''
+			} else {
+				link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.2}s`;
+			}
+		});
+		//Burger animation
+		burger.classList.toggle('toggle');
+	});
+
 }
 
+navSlide();
